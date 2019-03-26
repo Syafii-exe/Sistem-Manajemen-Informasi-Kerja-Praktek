e62c23735b538177548a7dc98bf3fefe265913ae
Aziz Wijaya <38089586+AzizWijaya25@users.noreply.github.com>
2019-03-26 12:01:25 +0700

    Update routes.php

--- sistem_sipil/application/config/routes.php
+++ sistem_sipil/application/config/routes.php
@@ -51,7 +51,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $route['default_controller'] = 'home';
 $route['administrator']='admin/login';
-$route['dosenview']='dosen/welcome';
+$route['dosenview']='dosen/contact';
 $route['artikel']='blog';
 $route['artikel']='blog/index';
 $route['artikel/(:any)']='blog/detail/$1';
