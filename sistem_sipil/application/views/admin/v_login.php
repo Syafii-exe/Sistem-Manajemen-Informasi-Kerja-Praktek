cf7d2a4fbea065bbb315948de927b4dcab769901
muhammad <37789994+fiayvv@users.noreply.github.com>
2019-04-06 22:11:23 +0700

    Login

--- sistem_sipil/application/views/admin/v_login.php
+++ sistem_sipil/application/views/admin/v_login.php
@@ -1,78 +1,109 @@
 <!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Log in</title>
-  <!-- Tell the browser to be responsive to screen width -->
-  <!-- Bootstrap 3.3.6 -->
-  <link rel="stylesheet" href="<?php echo base_url().'assets/bootstrap/css/bootstrap.min.css'?>">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="<?php echo base_url().'assets/font-awesome/css/font-awesome.min.css'?>">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="<?php echo base_url().'assets/dist/css/AdminLTE.min.css'?>">
-  <!-- iCheck -->
-  <link rel="stylesheet" href="<?php echo base_url().'assets/plugins/iCheck/square/blue.css'?>">
+<html lang="en">
 
+    <head>
+        <div>
+            <p><?php echo $this->session->flashdata('msg');?></p>
+         </div>
 
-</head>
-<body class="hold-transition login-page">
-<div class="login-box">
-  <div>
-   <p><?php echo $this->session->flashdata('msg');?></p>
-  </div>
-  <!-- /.login-logo -->
-  <div class="login-box-body">
-    <p class="login-box-msg"> <img width="320px;" src="<?php echo base_url().'assets/images/mylogo.png'?>"></p><hr/>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Login</title>
 
-    <form action="<?php echo site_url().'admin/login/auth'?>" method="post">
-      <div class="form-group has-feedback">
-        <input type="text" name="username" class="form-control" placeholder="Username" required>
-        <span class="glyphicon glyphicon-user form-control-feedback"></span>
-      </div>
-      <div class="form-group has-feedback">
-        <input type="password" name="password" class="form-control" placeholder="Password" required>
-        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-      </div>
-      <div class="row">
-        <div class="col-xs-8">
-          <div class="checkbox icheck">
-            <label>
-              <input type="checkbox"> Remember Me
-            </label>
-          </div>
+        <!-- CSS -->
+        <link rel="stylesheet" href="<?php echo base_url().'http://fonts.googleapis.com/css?family=Roboto:400,100,300,500'?>">
+        <link rel="stylesheet" href="<?php echo base_url().'asset/bootstrap/css/bootstrap.min.css'?>">
+        <link rel="stylesheet" href="<?php echo base_url().'asset/font-awesome/css/font-awesome.min.css'?>">
+		<link rel="stylesheet" href="<?php echo base_url().'asset/css/form-elements.css'?>">
+        <link rel="stylesheet" href="<?php echo base_url().'asset/css/style.css'?>">
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+        <!-- Favicon and touch icons -->
+        <link rel="shortcut icon" href="<?php echo base_url().'asset/ico/favicon.png'?>">
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo base_url().'asset/ico/apple-touch-icon-144-precomposed.png'?>">
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo base_url().'asset/ico/apple-touch-icon-114-precomposed.png' ?>">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo base_url().'asset/ico/apple-touch-icon-72-precomposed.png' ?>">
+        <link rel="apple-touch-icon-precomposed" href="<?php echo base_url().'asset/ico/apple-touch-icon-57-precomposed.png' ?>">
+
+    </head>
+
+    <body>
+
+		<!-- Top menu -->
+
+				<!-- Collect the nav links, forms, and other content for toggling -->
+
+			</div>
+		</nav>
+
+        <!-- Top content -->
+        <div class="top-content">
+        	
+            <div class="inner-bg">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-8 col-sm-offset-2 text">
+                            <h1><strong>Teknik Sipil</strong> </h1>
+                            <div class="description">
+                            	<p>
+
+                            	</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                    	<div class="col-sm-6 book">
+                    	<!-- 	<img src="#" alt="">-->
+                    	</div>
+                        <div class="col-sm-5 form-box">
+                        	<div class="form-top">
+                        		<div class="form-top-left">
+                        			<h3>Login</h3>
+                        		</div>
+                        		<div class="form-top-right">
+                        			<i class="fa fa-pencil"></i>
+                        		</div>
+                            </div>
+                                <form action="<?php echo site_url().'admin/login/auth'?>" method="post">
+                            <div class="form-bottom">
+			                    <form role="form" action="" method="post" class="registration-form">
+			                    	<div class="form-group">
+			                    		<label class="sr-only" for="form-first-name">Usernama</label>
+			                        	<input type="text" name="username" placeholder="Username" class="form-first-name form-control" id="form-first-name">
+			                        </div>
+			                        <div class="form-group">
+			                        	<label class="sr-only" for="form-last-name">Password</label>
+			                        	<input type="text" name="password" placeholder="Password" class="form-last-name form-control" id="form-last-name">
+			                        </div>
+			                        <button type="submit" class="btn">Login</button>
+			                    </form>
+		                    </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
         </div>
-        <!-- /.col -->
-        <div class="col-xs-4">
-          <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
-        </div>
-        <!-- /.col -->
-      </div>
-    </form>
 
 
-    <!-- /.social-auth-links -->
-    <hr/>
-   <!-- <p><center>Copyright <?php echo date('Y');?> by M Fikri Setiadi <br/> All Right Reserved</center></p> -->
-  </div>
-  <!-- /.login-box-body -->
-</div>
-<!-- /.login-box -->
+        <!-- Javascript -->
+        <script src="<?php echo base_url().'asset/js/jquery-1.11.1.min.js'?>"></script>
+        <script src="<?php echo base_url().'asset/bootstrap/js/bootstrap.min.js'?>"></script>
+        <script src="<?php echo base_url().'asset/js/jquery.backstretch.min.js'?>"></script>
+        <script src="<?php echo base_url().'asset/js/retina-1.1.0.min.js'?>"></script>
+        <script src="<?php echo base_url().'asset/js/scripts.js'?>"></script>
+        
+        <!--[if lt IE 10]>
+            <script src="asset/js/placeholder.js"></script>
+        <![endif]-->
 
-<!-- jQuery 2.2.3 -->
-<script src="<?php echo base_url().'assets/plugins/jQuery/jquery-2.2.3.min.js'?>"></script>
-<!-- Bootstrap 3.3.6 -->
-<script src="<?php echo base_url().'assets/bootstrap/js/bootstrap.min.js'?>"></script>
-<!-- iCheck -->
-<script src="<?php echo base_url().'assets/plugins/iCheck/icheck.min.js'?>"></script>
-<script>
-  $(function () {
-    $('input').iCheck({
-      checkboxClass: 'icheckbox_square-blue',
-      radioClass: 'iradio_square-blue',
-      increaseArea: '20%' // optional
-    });
-  });
-</script>
-</body>
+    </body>
+
 </html>
