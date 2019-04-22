d4b99d8facd34891dcc18a11d32219a62ca231e0
Aziz Wijaya <38089586+AzizWijaya25@users.noreply.github.com>
2019-04-22 15:44:09 +0700

    Update v_siswa.php

--- sistem_sipil/application/views/dosen/v_siswa.php
+++ sistem_sipil/application/views/dosen/v_siswa.php
@@ -50,13 +50,10 @@
                                   <a class="nav-link" href="<?php echo site_url('dosen/agenda');?>">Agenda</a>
                               </li>
                               <li class="nav-item">
-                                  <a class="nav-link" href="<?php echo site_url('dosen/download');?>">Download/Upload</a>
+                                  <a class="nav-link" href="<?php echo site_url('dosen/download');?>">Download Laporan</a>
                               </li>
                               <li class="nav-item">
-                                  <a class="nav-link" href="<?php echo site_url('dosen/galeri');?>">Dokumentasi</a>
-                              </li>
-                              <li class="nav-item">
-                                <a class="nav-link" href="<?php echo site_url('dosen/contact');?>">Logout</a>
+                                <a class="nav-link" href="<?php echo site_url('administrator');?>">Logout</a>
                               </li>
                         </ul>
                   </div>
@@ -116,8 +113,7 @@
                         <div class="sitemap">
                             <h3>Menu Utama</h3>
                             <ul>
-                                <li><a href="<?php echo site_url();?>">Home</a></li>
-                            <li><a href="<?php echo site_url('galeri');?>">Dokumentasi</a></li>
+								<li><a href="<?php echo site_url();?>">Home</a></li>
                             
                             </ul>
                         </div>
@@ -129,7 +125,7 @@
                               <li><a href="<?php echo site_url('siswa');?>">Mahasiswa </a></li>
                               <li><a href="<?php echo site_url('pengumuman');?>">Pengumuman</a></li>
                               <li><a href="<?php echo site_url('agenda');?>">Agenda</a></li>
-                              <li><a href="<?php echo site_url('download');?>">Download/Upload</a></li>
+                              <li><a href="<?php echo site_url('download');?>">Download Laporan</a></li>
                           </ul>
                       </div>
                     </div>
